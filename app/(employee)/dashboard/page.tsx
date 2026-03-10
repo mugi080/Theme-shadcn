3d7cfdca2d73e19330c25f6e94cf1f3a24addc5c
Mugi <mugi080@gmail.com>
2026-03-10 09:22:03 +0800

    kasalanan ni muhi

--- app/(employee)/dashboard/page.tsx
+++ app/(employee)/dashboard/page.tsx
@@ -1,33 +1,28 @@
 "use client"
 
-// import { useEffect } from "react"
-// import { useRouter } from "next/navigation"
-// this is part of it also for the verifaction of the token,
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { getToken } from "@/lib/api/personal-info/auth"
 
 export default function DashboardPage() {
+  const router = useRouter()
 
-  // const router = useRouter()
+  useEffect(() => {
+    const token = getToken()
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token")
-
-  //   if (!token) {
-  //     router.push("/login")
-  //   }
-  // }, [])
-
-  
-  // this is for the verifaction of the token, 
+    // if no token → redirect
+    if (!token) {
+      router.push("/login")
+    }
+  }, [router])
 
   return (
-    <>
-      <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="bg-muted/50 aspect-video rounded-xl" />
-          <div className="bg-muted/50 aspect-video rounded-xl" />
-          <div className="bg-muted/50 aspect-video rounded-xl" />
-        </div>
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
       </div>
-    </>
+    </div>
   )
 }
